da648642254cd7346de34042e5405f5290208ffa
Parth <parthbhadra14@gmail.com>
2023-06-02 13:29:01 +0530

    understanding this in vue

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const app = Vue.createApp({
     data: () => {
         return {
-            courseGoal: "Learn VueJS",
+            courseGoalA: "Learn VueJS",
+            courseGoalB: "Master VueJS",
             vueLink: "https://vuejs.org/"
         }
     },
     methods : {
         outputGoal() {
+            console.log('output goal function');
             const randomNumber = Math.random();
+            /* this keyword works differenly
+            in normal javascript - refers to the local object
+            but here in vue - the "data"'s properties are processed and attached to the global object
+            which our this then refers to */
             if(randomNumber < 0.5) {
-                return 'Learn Vue';
+                return this.courseGoalA;
             } else {
-                return 'Master Vue';
+                return this.courseGoalB;
             }
         }
     }
